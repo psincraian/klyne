0df52a810099f432e1bd928a90f737062cc23a0a
Petru Rares Sincraian <psincraian@outlook.com>
2025-08-11 23:14:31 +0200

    feat: improvements

--- app/src/models/user.py
+++ app/src/models/user.py
@@ -14,6 +14,7 @@ class User(Base):
     verification_token = Column(String, nullable=True)
     verification_token_expires = Column(DateTime(timezone=True), nullable=True)
     is_active = Column(Boolean, default=True, nullable=False)
+    is_admin = Column(Boolean, default=False, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
